a5001dd0aa8ba293636c4b44a3e9fc7773d9593f
AnhtuanDev <tuanho.design@outlook.com>
2021-11-18 17:08:24 +0700

    Thứ năm, 18 Tháng 11 năm 2021 17:08:24 +07

--- resources/views/livewire/partials/navbar.blade.php
+++ resources/views/livewire/partials/navbar.blade.php
@@ -26,7 +26,7 @@
 
                 @foreach($topMenu as  $menu)
                 <a 
-                    class="px-4 py-2 mt-2 text-sm text-primary-text capitalize md:mt-0
+                    class="px-4 py-2 mt-2 text-md font-semibold text-primary-text capitalize md:mt-0
                     hover:text-secondary-main focus:outline-none focus:shadow-outline"
                     href="{{ $menu['url'] }}">
                     {{ $menu['label'] }}
@@ -35,7 +35,7 @@
 
                 <div @click.away="open = false" class="relative" x-data="{ open: false }">
                     <button @click="open = !open"
-                        class="flex flex-row items-center w-full px-4 py-2 mt-2 text-sm 
+                        class="flex flex-row items-center w-full px-4 py-2 mt-2 text-md font-semibold 
                                     text-left text-primary-text md:w-auto md:inline md:mt-0
                                     hover:text-secondary-main focus:outline-none focus:shadow-outline">
 
@@ -46,12 +46,8 @@
                     </button>
                     <!-- dropdown menu -->
                     <div x-show="open" 
-                         x-transition:enter="transition ease-out duration-100"
-                         x-transition:enter-start="transform opacity-0 scale-95" 
-                         x-transition:enter-end="transform opacity-100 scale-100"
-                         x-transition:leave="transition ease-in duration-75" 
-                         x-transition:leave-start="transform opacity-100 scale-100" 
-                         x-transition:leave-end="transform opacity-0 scale-95" 
+                         x-transition.enter.scale.80.origin.top.right.duration.200ms
+                         x-transition.leave.scale.80.origin.top.right.duration.50ms
                          class="absolute right-0 z-30 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48" 
                          style="display: none">
 
@@ -61,7 +57,7 @@
 
                         <div class="px-2 py-2 bg-white rounded-md shadow">
                             @foreach($gioiThieu as $menu)
-                                <a class="block px-4 py-2 mt-2 capitalize text-sm text-primary-text
+                                <a class="block px-4 py-2 mt-2 capitalize text-md font-semibold text-primary-text
                                           md:mt-0 hover:text-secondary-main focus:outline-none focus:shadow-outline"
                                           href="{{ $menu['url'] }}">{{ $menu['label'] }}
                                 </a>
@@ -71,25 +67,27 @@
                 </div>
 
                 <div class="inline-flex items-center gap-2 list-none lg:ml-auto w-full sm:max-w-lg">
-                    <form action="" method="post" id="revue-form" name="revue-form" target="_blank" class="
-                                  p-1 transition duration-500 ease-in-out transform border2
+                    <form action="" 
+                          method="post"
+                          name="revue-form" target="_blank" class="
+                                  p-1 transition duration-500 ease-in-out transform
                                   bg-primary-light rounded-xl w-full sm:max-w-lg sm:flex">
 
-                        <div class="flex-1 min-w-0 revue-form-group">
+                        <div class="flex-1 min-w-0 block md:flex md:items-center">
                             <label for="search" class="sr-only">Search</label>
                             <input id="cta-email" type="text" class="
-                                       block w-full px-5 py-3 text-base text-primary-text placeholder-primary-dark transition
+                                       block w-full px-5 py-1 md:py-2 text-base text-primary-text placeholder-primary-dark transition
                                        duration-500 ease-in-out transform bg-transparent border border-transparent
                                        rounded-md focus:outline-none focus:border-transparent focus:ring-2
-                                       focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 " 
+                                       focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-dark " 
                                        placeholder="Nhập nội dung cần tìm ">
                         </div>
                         <div class="mt-4 sm:mt-0 sm:ml-3 revue-form-actions">
                             <button type="submit" value="Subscribe" name="member[subscribe]" id="member_submit" class="
-                                          block w-full px-5 py-3 text-base font-medium
+                                          block w-full px-5 py-1 md:py-2 text-base font-medium
                                           text-white bg-secondary-main border border-transparent rounded-lg
                                           shadow hover:bg-blue-500 focus:outline-none focus:ring-2
-                                          focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-300 sm:px-10
+                                          focus:ring-white focus:ring-offset-2 focus:ring-offset-primary-dark sm:px-10
                                           "> Tìm Kiếm </button>
                         </div>
                     </form>
@@ -99,37 +97,46 @@
     </div>
     
     <div class="bg-primary-main">
-        <div 
-            x-data="{ subMenu: null }" 
-            @click.outside="subMenu = null"
-            class="max-w-7xl mx-auto p-6 border-b border-priamry-dark">
-            <div 
-                @mouseover.away="subMenu = false"
-                class="flex flex-wrap justify-start items-center capitalize -ml-4">
+        <div x-data="{ subMenu: null }" 
+             @click.outside="subMenu = null"
+             @mouseover.away="subMenu = false"
+             class="max-w-7xl mx-auto px-6 py-2 border-b border-priamry-dark">
+
+            <div class="flex flex-wrap justify-start items-center capitalize -ml-4">
+
                 @foreach($mainMenu as $menu) 
                     @if($menu->child->count())
-                        <div class="relative">
-                            <div
-                                @mouseover.prevent="subMenu = {{ $menu->id }}" 
-                                @click.prevent="subMenu = {{ $menu->id }}" 
-                                class="main-menu-hover px-4 text-md font-semibold md:text-lg">
-                                {{ $menu->name}}
-                            </div>
-                            <template x-if="subMenu == {{ $menu->id }}">
-                                <div class="absolute w-64 p-6 left-0 top-0 mt-[2rem] on-top border-priamry-dark flex flex-col 
-                                    bg-primary-main space-y-4 rounded-lg">
-                                    @foreach($menu->child as $child) 
-                                    <a href="{{ route('home') }}" class="hover:text-secondary-dark duration-200 hover:font-bold text-md md:text-lg">
-                                        {{ $child->name }}
-                                    </a>
-                                    @endforeach
-                                </div>
-                            </template>
+
+                    <div class="relative">
+
+                        <div @mouseover.prevent="subMenu = {{ $menu->id }}" 
+                         @click.prevent="subMenu = {{ $menu->id }}" 
+                         class="main-menu-hover flex px-4 py-2 md:py-3 text-lg font-semibold md:text-xl">
+                         {{ $menu->name}}
                         </div>
+
+                            <div x-show="subMenu == {{ $menu->id }}"
+                                 x-transition.enter.scale.80.origin.top.left.duration.200ms
+                                 x-transition.leave.scale.80.origin.top.left.opacity.duration.50ms
+                                 class="absolute w-64 p-6 left-0 top-0 mt-[2.5rem] md:mt-[4rem] on-top border-b-4 border-secondary-main flex flex-col 
+                                        bg-primary-main space-y-4 shadow-lg rounded-b-lg"
+                                 style="display: none">
+
+                                        @foreach($menu->child as $child) 
+                                        <a href="{{ route('home') }}" class="hover:text-secondary-dark active:text-indigo-500 duration-200 hover:font-bold text-md md:text-lg">
+                                            {{ $child->name }}
+                                        </a>
+                                        @endforeach
+                            </div>
+
+                    </div>
+
                     @endif
                 @endforeach
+
             </div>
         </div>
+
     </div>
     
-</section>
+    </section>
