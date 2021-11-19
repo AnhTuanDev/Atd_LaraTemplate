8aadd2a545938fd819fea588999073f91ccde5ed
AnhtuanDev <tuanho.design@outlook.com>
2021-11-19 13:44:25 +0700

    Thứ sáu, 19 Tháng 11 năm 2021 13:44:25 +07

--- resources/views/components/shop/partials/navbar-link.blade.php
+++ resources/views/components/shop/partials/navbar-link.blade.php
@@ -4,11 +4,11 @@
     @click="open = '{{ $label }}'"
     type="button" 
     aria-expanded="false"
-    :class="{ 'border-secondary-main' : open === '{{ $label }}' } "
+    :class=" open === '{{ $label }}' ? 'border-b-2 border-secondary-main' : 'border-transparent' "
     class="
-        text-primary-text hover:text-secondary-main border-transparent
+        text-primary-text hover:text-secondary-main
         relative z-50 flex items-center transition-colors mx-8
-        ease-out duration-200 text-md font-semibold border-b-2 -mb-px pt-px
+        ease-out duration-200 text-md font-semibold -mb-px pt-px
     ">
         {{ $label ?? 'category name' }}
 
