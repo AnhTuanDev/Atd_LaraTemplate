8d649c6d7f464f8aa787ef06bd9527e7f840bbf5
AnhTuanDev <tuanho.design@outlook.com>
2022-01-21 10:17:31 +0700

    Thứ sáu, 21 Tháng 1 năm 2022 10:17:31 +07

--- resources/views/orchid/partials/footer.blade.php
+++ resources/views/orchid/partials/footer.blade.php
@@ -6,7 +6,7 @@
         <p class="small m-0">
             {{ __('Anh Tuấn: Design - Nhà phát triển Web và 3D Game ') }} - {{date('Y')}}<br>
             <a href="http://trillfa.com" target="_blank" rel="noopener">
-                Version: 1.01
+                {{ __('Version') }}: {{\Orchid\Platform\Dashboard::VERSION}}
             </a>
         </p>
     </div>
--- webpack.mix.js
+++ webpack.mix.js
@@ -4,13 +4,12 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  | Mix Asset Management
  |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
  */
+
+mix.disableSuccessNotifications();
+
 mix.setPublicPath('public_html/');
+
 mix.js('resources/js/app.js', 'public_html/js')
     .postCss('resources/css/app.css', 'public_html/css', [
         require('postcss-import'),
