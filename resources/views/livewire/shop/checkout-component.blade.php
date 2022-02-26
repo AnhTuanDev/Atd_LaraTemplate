77cffc6218d059baca9d5a944d530e3e1a88fb9b
AnhTuanDev <tuanho.design@outlook.com>
2022-02-26 10:24:44 +0700

    Sat Feb 26 10:24:44 AM +07 2022

--- resources/views/livewire/shop/checkout-component.blade.php
+++ resources/views/livewire/shop/checkout-component.blade.php
@@ -20,62 +20,57 @@
             "> Sản Phẩm Cùng Loại
         </h3>
     </div>
+
     {{-- Cart Content --}}
     <div class="w-full max-w-7xl mx-auto p-5 md:flex md:space-x-5">
         <!-- Cart list -->
         <div class="w-full md:w-1/2 rounded-md">
-            <div class="text-primary-text p-4 drop-shadow-lg rounded-md border border-primary-dark p-5">
+            <div class="text-primary-text p-4 drop-shadow-lg rounded-md border-2 border-primary-dark p-5">
                 @if($cartContent)
-                    @foreach($cartContent as $key => $item) 
-                        <div class="relative px-2 py-4 border-b border-dashed border-gray-300 text-primary-text flex flex-col">
-                            <div class="w-full flex items-center justify-between">
-                                <div class="flex items-center">
-                                    <img src="{{ $item->options->has('img') ? $item->options->img : '' }}" alt=""
-                                        class="w-16 rounded-md drop-shadow-md object-container object-center mr-2
-                                    ">
-                                    <div> @php $pro = App\Models\Shop\Product::findOrFail($item->id); @endphp
-                                        <a href="{{ route('shop.product.show', $pro) }}">
-                                            <h3 
-                                                class="text-md 
-                                                hover:text-secondary-dark text-primary-text
-                                                duration-150 font-medium 
-                                                leading-none uppercase
-                                                "> {{ $item->name }} 
-                                            </h3>
-                                        </a>
-                                
-                                        <div class="flex items-center mt-4 -ml-2 text-sm">
-                                        
-                                            <div for="color" class="w-5 h-5 bg-{{$item->options->color}} rounded-full ml-2"></div>
-                                
-                                            <label for="price" class="p-2 leading-none border-r border-gray-400"> Giá: {{ $item->price }} K</label>
-                                
-                                            <label for="qty" class="p-2 leading-none border-r border-gray-400"> Số lượng: {{ $item->qty }}</label>
-                                        
-                                            <label for="opti" class="flex items-center p-2 leading-none">{{ $item->options->has('size') ? $item->options->size : '' }}</label>
-                                
-                                        </div>
+                @foreach($cartContent as $key => $item) 
+                    <!-- Cart content -->
+                    <div class="relative px-2 py-4 border-b border-dashed border-gray-300 text-primary-text flex flex-col">
+                        <div class="w-full flex items-center justify-between">
+                            <div class="flex items-center">
+                                <img src="{{ $item->options->has('img') ? $item->options->img : '' }}" alt=""
+                                    class="w-16 rounded-md drop-shadow-md object-container object-center mr-2
+                                ">
+                                <div> @php $pro = App\Models\Shop\Product::findOrFail($item->id); @endphp
+                                    <a href="{{ route('shop.product.show', $pro) }}">
+                                        <h3 
+                                            class="text-md 
+                                            hover:text-secondary-dark text-primary-text
+                                            duration-150 font-medium 
+                                            leading-none uppercase
+                                            "> {{ $item->name }} 
+                                        </h3>
+                                    </a>
+                                    <div class="flex items-center mt-4 -ml-2 text-sm">
+                                        <div for="color" class="w-5 h-5 bg-{{$item->options->color}} rounded-full ml-2"></div>
+                                        <label for="price" class="p-2 leading-none border-r border-gray-400"> Giá: {{ $item->price }} K</label>
+                                        <label for="qty" class="p-2 leading-none border-r border-gray-400"> Số lượng: {{ $item->qty }}</label>
+                                        <label for="opti" class="flex items-center p-2 leading-none">{{ $item->options->has('size') ? $item->options->size : '' }}</label>
                                     </div>
                                 </div>
-                                <!-- Input quantity -->
-                                <input 
-                                    {{-- wire:model="quantity" --}}
-                                    :value="{{ $item->qty }}"
-                                    type="text" 
-                                    class="border border-primary-main rounded-md focus:outline-none 
-                                    focus:border-transparent focus:ring-2
-                                    w-10 text-md leading-none inline-flex focus:ring-secondary-dark
-                                    items-center text-center font-semibold
-                                    ">
-                                <!-- Button remove cart item -->
-                                <button wire:click.prevent="cartRemove( '{{$item->rowId }}' )" 
-                                     class="p-1 rounded-lg bg-secondary-dark hover:drop-shadow-md hover:bg-primary-dark duration-50 mt-2">
-                                    <x-orchid-icon path="trash" class="text-sm font-bold text-secondary-text"/>
-                                </button>
                             </div>
-
+                            <!-- Input quantity -->
+                            <input 
+                                {{-- wire:model="quantity" --}}
+                                :value="{{ $item->qty }}"
+                                type="text" 
+                                class="border border-primary-main rounded-md focus:outline-none 
+                                focus:border-transparent focus:ring-2
+                                w-10 text-md leading-none inline-flex focus:ring-secondary-dark
+                                items-center text-center font-semibold
+                                ">
+                            <!-- Button remove cart item -->
+                            <button wire:click.prevent="cartRemove( '{{$item->rowId }}' )" 
+                                 class="p-1 rounded-lg bg-secondary-dark hover:drop-shadow-md hover:bg-primary-dark duration-50 mt-2">
+                                <x-orchid-icon path="trash" class="text-sm font-bold text-secondary-text"/>
+                            </button>
                         </div>
-                    @endforeach
+                    </div>
+                @endforeach
                 @endif
 
                 @if(!$cartCount)
@@ -84,74 +79,66 @@
                         <h3 class="w-full text-sm ml-2"> Chưa có sản phẩm trong giỏ  </h3>
                     </div>
                 @endif
+
                 @if($cartCount)
-                    <div class="w-full 
-                        max-w-7xl mx-auto flex items-center 
-                        justify-end space-x-4 mt-4
-                        ">
-                        <a href="{{ route('shop.cart.checkout') }}">
+                    <div class="w-full flex items-center mt-4">
+                        <a href="{{ route('shop.cart.checkout') }}" class="w-full">
                             <button 
-                                class="bg-secondary-main 
-                                border border-secondary-main hover:bg-primary-main
+                                class="bg-primary-light w-full 
+                                border border-primary-main hover:bg-primary-main
                                 px-4 py-2 rounded-md text-sm md:text-md 
                                 md:font-semibold duration-150"
-                                >Update
+                                >Cập Nhật
                             </button>
                         </a>
                     </div>
                 @endif
-
             </div>
         </div>
+
         <!-- Cart action -->
-        @if($cartCount)
-        <div 
-            class="w-full rounded-md 
-            border-primary-dark md:w-1/2 mt-4 md:mt-0 p-4 border-2 border 
-            ">
-            {{ \Cart::subtotal() }}
-            <div class="w-full 
-                max-w-7xl mx-auto flex items-center 
-                space-x-4 justify-between 
-                ">
-                <a href="{{ route('shop.cart.checkout') }}">
+        <div class="w-full md:w-1/2 rounded-md">
+            @if($cartCount)
+                <div class="rounded-md w-full border-primary-dark mt-4 md:mt-0 p-4 border-2 border">
+                    {{ \Cart::subtotal() }}
+                    <div class="w-full mt-6">
+                        <textarea id="" name="note" rows="5"
+                            placeholder="Ghi chú"
+                            class="w-full rounded-md border border-primary-dark
+                            placeholder:capitalize placeholder:text-lg placeholder:text-gray-700
+                            focus:ring-secondary-dark focus:ring-2 text-md 
+                            focus:outline-none placeholder:font-semibold
+                            focus:border-transparent leading-none 
+                            ">
+                        </textarea>
+                    </div>
+                </div>
+                <div class="w-full mt-4 flex items-center">
                     <button 
-                        class="bg/checkout-secondary-main 
-                        border border-secondary-main hover:bg-primary-main
+                        wire:click="destroyCart" 
+                        class="w-full bg-secondary-main uppercase
+                        border border-secondary-main hover:bg-secondary-dark
+                        px-4 py-2 rounded-md text-md md:text-md 
+                        font-semibold duration-150"
+                        >Thanh Toán
+                    </button>
+                </div>
+            @endif
+            <!-- Tiếp tục mua hàng -->
+            <div class="w-full max-w-7xl flex justify-center items-center mt-4">
+                <a href="{{ route('shop.index') }}" class="w-full">
+                    <button 
+                        class="bg-primary-light w-full 
+                        border border-primary-main hover:bg-primary-main
                         px-4 py-2 rounded-md text-sm md:text-md 
                         md:font-semibold duration-150"
-                        >Xem Giỏ Hàng
+                        >Tiếp Tục Mua Hàng
                     </button>
                 </a>
-                <button 
-                    wire:click="destroyCart" 
-                    class="bg-primary-dark
-                    border border-secondary-main hover:bg-primary-main
-                    px-4 py-2 rounded-md text-sm md:text-md 
-                    font-semibold duration-150"
-                    >Xóa Giỏ
-                </button>
             </div>
+            <!-- End Tiếp tục mua hàng -->
         </div>
-        @endif
-        <!-- End Cart action -->
     </div>
     {{-- Ende   Cart Content --}}
-
-    <!-- Tiếp tục mua hàng -->
-    @if(!$cartCount)
-        <div class="w-full max-w-7xl mx-auto flex items-center">
-            <a href="{{ route('shop.index') }}">
-                <button 
-                    class="bg-secondary-main 
-                    border border-secondary-main hover:bg-primary-main
-                    px-4 py-2 rounded-md text-sm md:text-md 
-                    md:font-semibold duration-150"
-                    >Tiếp Tục Mua Hàng
-                </button>
-            </a>
-        </div>
-    @endif
-    
 
 </section>
--- resources/views/livewire/shop/partials/cart-component.blade.php
+++ resources/views/livewire/shop/partials/cart-component.blade.php
@@ -26,116 +26,91 @@
         x-transition:enter.origin.right.top.duration.200ms
         x-transition:leave.origin.right.top.duration.150ms
         style="display: none"
-        class="fixed 
-        w-screen md:w-96 
-        h-screen z-[9999] 
-        bg-primary-dark 
-        overflow-y-auto
-        drop-shadow-lg 
-        right-0 top-0 
-        bg-opacity-95
-        bg-blur-lg
+        class="fixed w-screen md:w-96 
+        h-screen z-[9999] bg-primary-dark 
+        overflow-y-auto drop-shadow-md
+        right-0 top-0 bg-opacity-[95%]
+        backdrop-blur-lg border-l
+        border-opacity-30
+        border-gray-700 
         ">
 
-        <div class="w-full flex items-center justify-between p-6 border-b border-gray-300">
+        <div class="w-full flex 
+            items-center justify-between 
+            p-6 border-b border-gray-700
+            border-opacity-20
+            ">
             <h3 class="text-xl font-semibold text-primary-text leading-none">Giỏ Hàng</h3>
             <x-orchid-icon path="close" class="text-2xl font-bold text-primary-text"/>
         </div>
 
         <div x-data
             @click.outside="$store.shop.overflow = false; $store.shop.toggleSlideCart()"
-            class="text-primary-text 
-            p-4 drop-shadow-lg 
-            border-b
+            class="text-primary-text px-4 pt-4 pb-6 drop-shadow-lg 
+            border-b border-gray-700 border-opacity-20
             ">
             @if($cartContent)
                 @foreach($cartContent as $key => $item) 
                     <div 
-                        class="relative 
-                        border-dashed 
-                        px-2 py-4 border-b
-                        border-gray-300 
-                        text-primary-text 
-                        flex flex-col
+                        class="relative border-dashed border-opacity-30
+                        px-2 py-4 border-b border-gray-700
+                        text-primary-text flex flex-col
                         @if($cartContent) 
                             translate-open 
                         @else
                             translate-leave 
                         @endif
                         ">
-
                        <button wire:click.prevent="cartRemove( '{{$item->rowId }}' )" 
-                            class="p-1 rounded-lg 
-                            absolute top-0 right-0 z-[9999] 
-                            hover:bg-secondary-main 
-                            hover:drop-shadow-md 
-                            bg-secondary-dark 
+                            class="p-1 rounded-lg absolute 
+                            top-0 right-0 z-[9999] hover:bg-secondary-main 
+                            hover:drop-shadow-md bg-secondary-dark 
                             duration-50 mt-2
-                            ">
-                           <x-orchid-icon path="trash" 
-                               class="text-sm font-bold text-secondary-text"/>
+                            "><x-orchid-icon path="trash" class="text-sm font-bold text-secondary-text"/>
                        </button>
-
                         <div class="flex items-center">
                             <img src="{{ $item->options->has('img') ? $item->options->img : '' }}" alt=""
                                 class="w-16 rounded-md drop-shadow-md object-container object-center mr-2">
                             <div>
                                 <h3 class="text-md font-medium leading-none uppercase"> {{ $item->name }} </h3>
-
                                 <div class="flex items-center mt-4 -ml-2 text-sm">
-                                
                                     <div for="color" class="w-5 h-5 bg-{{$item->options->color}} rounded-full ml-2"></div>
-
                                     <label for="price" class="p-2 leading-none border-r border-gray-400"> Giá: {{ $item->price }} K</label>
-
                                     <label for="qty" class="p-2 leading-none border-r border-gray-400"> Số lượng: {{ $item->qty }}</label>
-                                
                                     <label for="opti" class="flex items-center p-2 leading-none">{{ $item->options->has('size') ? $item->options->size : '' }}</label>
-                                
                                 </div>
                             </div>
                         </div>
-
                     </div>
                 @endforeach
             @endif
-
             @if(!$cartCount)
                 <div class="flex items-center text gray-500">
                     <x-orchid-icon path="bell" />
                     <h3 class="w-full text-sm ml-2"> Chưa có sản phẩm trong giỏ  </h3>
                 </div>
             @endif
-
             @if($cartCount)
-                <div class="w-full flex items-center space-x-4 justify-between mt-4">
+                <div class="w-full flex items-center space-x-4 justify-between mt-6">
                     <a href="{{ route('shop.cart.checkout') }}">
-                        <button 
-                            class="bg-secondary-main 
-                            border border-secondary-main
-                            hover:bg-primary-main
-                            px-4 py-2 rounded-md 
-                            text-sm md:text-md 
-                            md:font-semibold
-                            duration-150"
+                        <button class="bg-secondary-main 
+                            border border-secondary-main hover:bg-primary-main
+                            px-4 py-2 rounded-md text-sm md:text-md 
+                            md:font-semibold duration-150"
                             >Xem Giỏ Hàng
                         </button>
                     </a>
                     <button 
                         wire:click="destroyCart" 
                         @click.prevent="$store.shop.toggleSlideCart()"
-                        class="bg-primary-dark
-                        border border-secondary-main
-                        hover:bg-primary-main
-                        px-4 py-2 rounded-md 
-                        text-sm md:text-md 
-                        font-semibold
-                        duration-150"
-                        >Xóa Giỏ
+                        class="bg-primary-dark border 
+                        border-secondary-main hover:bg-primary-main
+                        px-4 py-2 rounded-md text-sm md:text-md 
+                        font-semibold duration-150
+                        ">Xóa Giỏ
                     </button>
                 </div>
             @endif
-
         </div>
     </div>
 </div>
